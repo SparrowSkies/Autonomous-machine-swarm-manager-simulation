cc1bcca105d5732fe8653043212cfeacab00e5cb
SparrowSkies <155788210+SparrowSkies@users.noreply.github.com>
2026-02-15 07:40:49 -0800

    added an addition feature to number.h

--- number.h
+++ number.h
@@ -21,6 +21,12 @@ struct namedNumber
     long long decimal;
 };
 
+void addNumber(namedNumber& numToChange, namedNumber addNum)
+{
+    numToChange.whole = numToChange.whole + addNum.whole;
+    numToChange.decimal = numToChange.decimal + addNum.decimal;
+}
+
 
 
 #endif //AUTONUMOS_MACHINE_SWARM_MANAGER_NUMBER_H
