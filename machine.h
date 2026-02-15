2e2ebac516ce483604d61b887b34c7f591e59b35
SparrowSkies <155788210+SparrowSkies@users.noreply.github.com>
2026-02-15 13:15:29 -0800

    major changes that included deleting some unneeded files, and changing other files to make more sense

--- machine.h
+++ machine.h
@@ -1,17 +1,60 @@
-#ifndef AUTONUMOS_MACHINE_SWARM_MANAGER_MACHINE_H
-#define AUTONUMOS_MACHINE_SWARM_MANAGER_MACHINE_H
-#import <vector>
-#import <string>
-#include "number.h"
+#include <vector>
+#include <string>
+#include <variant>
 
 
+enum class MachineRole
+{
+    Dozer,
+    Charger
+};
+
+struct namedNumber
+{
+    std::string NAME;
+    double numb;
+};
+
+struct machinePosition
+{
+    double X;
+    double Y;
+    double Z;
+};
+
+struct machineForwardDirection
+{
+    double X;
+    double Z;
+};
+
+struct dozerData
+{
+    std::string machineName;
+    namedNumber dataSensors[0];
+    machinePosition pos;
+    machineForwardDirection dir;
+};
+
+struct chargerData
+{
+    std::string machineName;
+    namedNumber dataSensors[0];
+    machinePosition pos;
+    machineForwardDirection dir;
+};
 
 class machine
 {
 public:
-    std::vector<namedNumber> dataSensors;
-    machine(){}
+
+    MachineRole MR;
+    std::variant<dozerData, chargerData> meta;
+
+    machine(const dozerData& d) : meta(d) {}
+
+    machine(const chargerData& c) : meta(c) {}
+
+    ~machine() {}
 };
 
-
-#endif //AUTONUMOS_MACHINE_SWARM_MANAGER_MACHINE_H
--- main.cpp
+++ main.cpp
@@ -1,27 +1,14 @@
 #import <iostream>
 #import <string>
 #include "machine.h"
-#include "number.h"
+
 
 int main()
 {
-    machine newMachine;
-    namedNumber newNumber;
-    newNumber.NAME = "testSensor";
-    newNumber.numb = -0.223;
-    newMachine.dataSensors.push_back(newNumber);
+    dozerData d;
+    d.machineName = "Dozer-1";
 
-    for (int i = 0; i < newMachine.dataSensors.size(); i++)
-    {
-        namedNumber target = newMachine.dataSensors[i];
-        if (target.NAME == "testSensor")
-        {
-            numberStruct newNumber1
-            {
-                10.23444
-            };
+    machine dozer(d);
 
-            std::cout << target.numb << " " << target.numb + newNumber1.numb;
-        }
-    }
+    std::cout << std::get<dozerData>(dozer.meta).machineName;
 }
